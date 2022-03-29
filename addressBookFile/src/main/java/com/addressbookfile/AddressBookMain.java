426fa359f4f6a1129ddefba059028c1aff1924fb
deven7818 <deven7818@gmail.com>
2022-03-30 00:01:52 +0530

    Read or write address book with persons contact into file

---
+++ addressBookFile/src/main/java/com/addressbookfile/AddressBookMain.java
@@ -0,0 +1,110 @@
+package com.addressbookfile;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 1. Creating new Contact in address book with first and last names, address,city,
+ * state, zip, phone number and email.
+ * 2. adding new contact to address book
+ * 3.editing contact with name 
+ * 4. Delete person using person's name
+ * 5. Add multiple persons in address book
+ * 6. add multiple address book with unique Name 
+ * 7. check if duplicate entry of same person in particular address book
+ * 8. Search person in city or state across multiple address book
+ * 9. view person by city or state
+ * 10. Get number of contact i.e. count by city or state 
+ * 11. sort the entries in the address book alphabetically by Person’s name
+ * 12. sort the entries in the address book by City, State, or Zip
+ */
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program...!!!");
+		Scanner sc = new Scanner(System.in);
+		
+		/**
+		 * creating object of AddNewContact
+		 */
+		AddNewContact addNew = new AddNewContact();
+		
+		/**
+		 * creating object of UniqueMultipleAddressBook for adding Unique name address book
+		 */
+		UniqueMultipleAddressBook unique = new UniqueMultipleAddressBook();
+
+		while (true) {
+			System.out.println("Enter \n    1. Add the new AddressBook \n "
+										+  "2. Add new contact \n "
+										+  "3. Edit the contact \n "
+										+  "4. Delete the contact \n "
+										+  "5. Delete the AddressBook \n "
+										+  "6. Print the AddressBook \n "
+										+  "7. Print the contacts \n "
+										+  "8. Search By city \n "
+										+  "9. Search By state \n "
+										+  "10. View Person By City \n "
+										+  "11. view Person By state \n "
+										+  "12. Count people \n "
+										+  "0. Exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+			
+			case 1:
+				unique.addAddress();
+				break;
+			case 2:
+				unique.addContact();
+				break;
+
+			case 3:
+				unique.editContact();
+				break;
+
+			case 4:
+				unique.deleteContact();
+				break;
+				
+			case 5:
+				unique.deleteAddressBook();
+				break;
+			case 6:
+				unique.printBook();
+				break;
+			case 7:
+				unique.printContacts();
+				break;
+			case 8:
+				unique.searchByCity();
+				break;
+			case 9:
+				unique.searchByState();
+				break;
+			case 10:
+				unique.viewPerson(AddNewContact.personByCity);
+				break;
+			case 11:
+				unique.viewPerson(AddNewContact.personByState);
+				break;
+				
+			case 12:
+				System.out.println("Enter \n1.Count By City \n2. Count By state");
+				int num = sc.nextInt();
+				if(num ==1)
+					unique.countPeople(AddNewContact.personByCity);
+					else
+						unique.countPeople(AddNewContact.personByState);
+					break;
+				
+			case 0:
+				System.exit(8);
+				break;
+			default:
+				System.out.println("Wrong Input");
+			}
+		}
+		
+	}
+	
+}
---
+++ addressBookFile/src/main/java/com/addressbookfile/UniqueMultipleAddressBook.java
@@ -0,0 +1,180 @@
+package com.addressbookfile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 
+ * Added Multiple address book with each address book has unique name 
+ *
+ */
+public class UniqueMultipleAddressBook {
+	
+	/**
+	 * Hashmap to add multiple address book
+	 */
+	Map<String, AddNewContact> addressMap = new HashMap<>();
+	List<Contact> contacts = new ArrayList<Contact>();
+	Scanner sc = new Scanner(System.in);
+	
+	public void addAddress() {
+		System.out.println("Enter New Address book name :");
+		Scanner sc = new Scanner(System.in);
+		String bookname = sc.next();
+		if(addressMap.containsKey(bookname)) {
+			System.out.println("Address book with given name is exist. Please enter new Name ");
+			addAddress();
+		}
+		else {
+			AddNewContact addNewContact = new AddNewContact();
+			addressMap.put(bookname, addNewContact);
+			System.out.println("Address Book Successfully Added");
+		}
+		sc.close();
+	}
+	
+	/**
+	 * Method to add contact in address book
+	 */
+	public void addContact() {                                          
+		System.out.println("Enter the name of Address book to add the contact.");
+		Scanner sc = new Scanner(System.in);
+		String newContact = sc.nextLine();
+		AddNewContact addContact = addressMap.get(newContact);          
+		if (addContact == null) {
+			System.out.println("No book found");
+
+		} else {
+			addressMap.get(newContact).addContact();                         // if present then add it to hashMap
+		}
+		sc.close();
+	}
+	/**
+	 * Method to edit contact in address book
+	 */
+	public void editContact() {
+		System.out.println("Enter Name of Contact you want to edit : ");
+		Scanner sc = new Scanner(System.in);
+		String editName = sc.next();
+		if (addressMap.containsKey(editName)) {
+			addressMap.get(editName).editContact();                               // calling the edit contact method to edit contacts
+		} else {
+			System.out.println("Contact doesn't exist, Please enter correct name.");
+			editContact();
+		}
+		sc.close();
+	}
+
+	/**
+	 * Method to delete Contact in address book
+	 */
+
+	public void deleteContact() {
+		System.out.println("Enter Name of Contact you want to delete : ");
+		Scanner sc = new Scanner(System.in);
+		String bookName = sc.next();
+		if (addressMap.containsKey(bookName)) {
+			addressMap.get(bookName).deleteContact();                                            // we call the deleteContact function to delete the contact
+		} else {
+			System.out.println("AddressBook doesn't exist, Please enter correct name.");
+			deleteContact();
+		}
+		sc.close();
+	}
+
+
+	
+	/**
+	 * Method to delete Address book
+	 */
+	public void deleteAddressBook() {
+		System.out.println("Enter Name of Address Book you want to delete: ");
+		Scanner sc = new Scanner(System.in);
+		String bookName = sc.next();
+		if (addressMap.containsKey(bookName)) {                                       //we use containsKey to check if addressBook present 
+			addressMap.remove(bookName);                                                 // and use remove fun to remove the book
+		} else {
+			System.out.println("AddressBook doesn't exist, Please enter correct name.");
+			deleteAddressBook();
+		}
+		sc.close();
+	}
+	
+	/**
+	 * Method to print address book
+	 */
+	public void printBook() {
+		System.out.println("Address books are");
+		for (String i : addressMap.keySet()) {                                     // we use keySet to get all the keys and display it
+			System.out.println(i);
+		}
+	}
+
+	/**
+	 * Method to print contact list in address book
+	 */
+	public void printContacts() {
+		for (String i : addressMap.keySet()) {
+			System.out.println(i);
+			System.out.println(addressMap.get(i).contacts);                     //we print the values of the key
+		}
+		System.out.println(" ");
+	}
+	
+	/**
+	 * Search personin contact by city Name
+	 */
+    public void searchByCity() {
+		System.out.println("Enter the name of the City to search the persons : ");
+		String cityName = sc.next();
+		for (String i : addressMap.keySet()) {
+		List<Contact> arr = addressMap.get(i).contacts;
+		arr.stream().filter(person -> person.getCity().equals(cityName)).forEach(person -> System.out.println(person.getFirstName()));
+      }		
+    }
+
+    /**
+     * Search Person By State
+     */
+    public void searchByState() {
+    	
+    	System.out.println("Enter the name of the State to Search persons : ");
+    	String stateName = sc.next();
+    	for (String i : addressMap.keySet()) {
+    	List<Contact>	arr = addressMap.get(i).contacts;
+    	arr.stream().filter(person -> person.getState().equals(stateName)).forEach(person -> System.out.println(person.getFirstName()));
+      }		
+    }
+    
+    /**
+     * View person by city or state 
+     * 
+     */
+    public void viewPerson(HashMap<String, ArrayList<Contact>> addressMap) {
+    	List<Contact> contacts;
+    	for (String name : addressMap.keySet()) {
+    		System.out.println("People residing in: " + name);
+    		contacts = addressMap.get(name);
+    		for (Contact contact : contacts) {
+    			System.out.println(contact);
+    		}
+    	}
+
+    }
+    
+    /*
+     * Method to count People living in city or state
+     */
+    public void countPeople(HashMap<String, ArrayList<Contact>> list) {
+    	System.out.println("Enter name ");
+    	String result = sc.next();
+    	long count =  list.values().stream()
+    			.map(region -> region.stream().filter(person -> person.getState().equals(result) || person.getCity().equals(result)))
+    			.count();
+    				
+    	System.out.println("Number of people living in " + result + "are : "+count);
+    }
+}
